c1f75c33d34ddd4279b0d8ae4db4238e4402ff23
EC2 Default User <ec2-user@ip-172-31-44-106.ec2.internal>
2021-02-06 17:38:53 +0000

    Updated the css file and html file for basic preview. Also added a for loop in music.py file to fetch out all artist included in song.

--- music.py
+++ music.py
@@ -9,7 +9,7 @@ AUTH_URL="https://accounts.spotify.com/api/token"
 Random_artist = ["https://api.spotify.com/v1/artists/0Tgdv4JlRUoXWfGTrWgY1m/top-tracks?market=US",
                  "https://api.spotify.com/v1/artists/008PpLcKUtVXle6JSwkq3I/top-tracks?market=US",
                  "https://api.spotify.com/v1/artists/3TVXtAsR1Inumwj472S9r4/top-tracks?market=US"]
-
+                 
 client_creds = f"{os.getenv('client_id')}:{os.getenv('client_secret')}"
 client_creds_b64 = base64.b64encode(client_creds.encode())
 
@@ -33,13 +33,24 @@ def get_data():
     response = requests.get(random.choice(Random_artist), headers=headers)
     data = response.json()
     
-    arr = []
+    rtn_array = []
     
     for i in (data['tracks']):
         song_name = i['name']
-        song_artist = i['album']['artists'][0]['name']
         song_link = i['preview_url']
         song_image = i['album']['images'][0]['url']
+        artist_array = []
+        for j in range(len(i['album']['artists'])):
+            artist_array.append(i['album']['artists'][j]['name'])
+        song_artist = str(artist_array)[1:-1].replace("'", "")
         array = [song_name,song_artist,song_link,song_image]
-        arr.append(array)
-    return random.choice(arr)
+        rtn_array.append(array)
+    return random.choice(rtn_array)
+
+'''
+response = requests.get(test, headers=headers)
+data = response.json()
+for i in (data['tracks']):
+    for e in range(len(i['album']['artists'])):
+        print(i['album']['artists'][e]['name'])
+'''
